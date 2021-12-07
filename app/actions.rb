aca4e82ee1d0e675febbf64328e6a12db5323f30
Aron MacDonald <aron.macdonald@shopify.com>
2021-12-07 01:03:10 +0000

    Mid week6 D1

--- app/actions.rb
+++ app/actions.rb
@@ -76,3 +76,14 @@ get '/finstagram_posts/:id' do
     @finstagram_post = FinstagramPost.find(params[:id])
     erb(:"finstagram_posts/show")
 end
+
+post '/comments' do
+    text = params[:text]
+    finstagram_post_id = params[:finstagram_post_id]
+  
+    comment = Comment.new({ text: text, finstagram_post_id: finstagram_post_id, user_id: current_user.id })
+  
+    comment.save
+  
+    redirect(back)
+  end
